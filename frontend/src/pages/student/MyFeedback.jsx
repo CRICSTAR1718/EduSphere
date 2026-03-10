5c4e0871f6944efe543e406971a50297cf96dc87
Vaibhav Vaid <142966207+vaibhavvaid14@users.noreply.github.com>
2026-03-11 00:24:00 +0530

    Added MyFeedback component.jsx

---
+++ frontend/src/pages/student/MyFeedback.jsx
@@ -0,0 +1,11 @@
+import FeedbackHistory from "../../components/student/FeedbackHistory";
+
+const MyFeedback = () => {
+  return (
+    <div className="p-6">
+      <FeedbackHistory />
+    </div>
+  );
+};
+
+export default MyFeedback;
